5caf37c5a4187ffbded802871b69da11c1b3789d
Sergey Popov <poserg@gmail.com>
2014-03-19 23:38:07 +0400

    Added query for creating database

--- db_utils.py
+++ db_utils.py
@@ -1,2 +1,41 @@
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, Column, Integer, String, MetaData, ForeignKey
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
 
+engine = create_engine('sqlite:///store.db')
+
+Base = declarative_base()
+
+class Image(Base):
+    __tablename__ = 'images'
+    id = Column(Integer, primary_key = True)
+    name = Column(String)
+    
+    def __init__(self, name):
+        self.name = name
+        
+    def __repr__(self):
+        return "<Image('%s')>" % (self.name)
+        
+class Group(Base):
+    __tablename__ = 'groups'
+    id = Column(Integer, primary_key = True)
+    name = Column(String)
+    
+    def __init__(self, name):
+        self.name = name
+        
+    def __repr__(self):
+        return "<Group('%s')>" % (self.name)
+        
+metadata = Base.metadata
+metadata.create_all(engine)
+
+Session = sessionmaker(bind = engine)
+image = Image('Hello')
+session = Session()
+session.add(image)
+session.commit()
+
+t = session.query(Image).order_by(Image.id)
+print (t)
--- graphic/graphic.py
+++ graphic/graphic.py
@@ -1,1 +1,14 @@
 # -*- coding: utf-8 -*-
+
+from PIL import Image
+
+#convert $ftmp -gamma 0.454545 -geometry "$maxfull[0]x$maxfull[1]>" -print %w\n%h -gamma 2.2 +profile !icc* -quality $imgq "$out/$fimg" 
+
+# Generate main image
+# convert -quality 90 -resize 1600x1067 IMG_1203.JPG my_out.jpg
+
+# Thumbs
+# convert -quality 90 -resize 168x112 IMG_1203.JPG my_out.jpg
+
+# Blur
+# convert my_out.jpg -virtual-pixel Mirror -gaussian-blur 0x8 -scale 252x336 -quality 90 my_out_blur.jpg
