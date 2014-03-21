ab890717fe7d36520e4c05406896d1b3dab9e8cf
Sergey Popov <poserg@gmail.com>
2014-03-21 17:02:40 +0400

    Add database's entities

--- db_utils.py
+++ db_utils.py
@@ -7,7 +7,7 @@ engine = create_engine('sqlite:///store.db')
 Base = declarative_base()
 
 class Image(Base):
-    __tablename__ = 'images'
+    __tablename__ = 'image'
     id = Column(Integer, primary_key = True)
     name = Column(String)
     
@@ -18,7 +18,7 @@ class Image(Base):
         return "<Image('%s')>" % (self.name)
         
 class Group(Base):
-    __tablename__ = 'groups'
+    __tablename__ = 'group'
     id = Column(Integer, primary_key = True)
     name = Column(String)
     
@@ -27,7 +27,42 @@ class Group(Base):
         
     def __repr__(self):
         return "<Group('%s')>" % (self.name)
-        
+
+class ImageGroup(Base):
+    __tablename__ = 'image_group'
+    image_id = Column(Integer, ForeignKey('image.id'), primary_key = True)
+    group_id = Column(Integer, ForeignKey('group.id'), primary_key = True)
+
+    def __init__(self, image_id, group_id):
+        self.image_id = image_id
+        self.group_id = group_id
+
+class OperationType(Base):
+    __tablename__ = 'operation_type'
+    id = Column(Integer, primary_key = True)
+    name = Column(String)
+
+    def __init__(self, name):
+        self.name = name
+
+class Operation(Base):
+    __tablename__ = 'operation'
+    id = Column(Integer, primary_key = True)
+    name = Column(String)
+    operation_type_id = Column(Integer, ForeignKey('operation_type.id'))
+
+    def __init__(self, name, operation_type_id):
+        pass
+
+class OperationGroup(Base):
+    __tablename__ = 'operation_group'
+    operation_id = Column(Integer, ForeignKey('operation.id'), primary_key = True)
+    group_id = Column(Integer, ForeignKey('group.id'), primary_key = True)
+
+    def __init__(self, operation_id, group_id):
+        self.operation_id = operation_id
+        self.group_id = group_id
+
 metadata = Base.metadata
 metadata.create_all(engine)
 
