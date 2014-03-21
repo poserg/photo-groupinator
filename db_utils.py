31847aab57a2c86eea6f2b482c056165fcea9a99
Sergey Popov <poserg@gmail.com>
2014-03-22 01:49:55 +0400

    Divide db_utils

--- db_utils.py
+++ db_utils.py
@@ -1,76 +1,58 @@
-from sqlalchemy import create_engine, Column, Integer, String, MetaData, ForeignKey
-from sqlalchemy.orm import sessionmaker
-from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import create_engine, Column, Integer, String, MetaData, ForeignKey, MetaData, Table
+from sqlalchemy.orm import sessionmaker, mapper
+from entities import *
+
+def create_db(engine):
+    metadata = MetaData()
+    
+    image_table = Table('image', metadata,
+                        Column('id', Integer, primary_key = True),
+                        Column('name', String)
+    )
+    
+    group_table = Table('group', metadata, 
+                        Column('id', Integer, primary_key = True),
+                        Column('name', String)
+    )
+
+    image_group_table = Table('image_group', metadata, 
+                              Column('image_id', Integer, ForeignKey('image.id'), primary_key = True),
+                              Column('group_id', Integer, ForeignKey('group.id'), primary_key = True)
+    )
+
+    operation_type_table = Table('operation_type', metadata,
+                                 Column('id', Integer, primary_key = True),
+                                 Column('name', String)
+    )
+
+    operation_table = Table('operation', metadata, 
+                            Column('id', Integer, primary_key = True),
+                            Column('name', String),
+                            Column('operation_type_id', Integer, ForeignKey('operation_type.id'))
+    )
+
+    operation_group_table = Table('operation_group', metadata, 
+                                  Column('operation_id', Integer, ForeignKey('operation.id'), primary_key = True),
+                                  Column('group_id', Integer, ForeignKey('group.id'), primary_key = True)
+    )
+
+    metadata.create_all(engine)
+    mapper(Image, image_table)
+    mapper(Group, group_table)
+    mapper(ImageGroup, image_group_table)
+    mapper(OperationType, operation_type_table)
+    mapper(Operation, operation_table)
+    mapper(OperationGroup, operation_group_table)
+
 
 engine = create_engine('sqlite:///store.db')
 
-Base = declarative_base()
-
-class Image(Base):
-    __tablename__ = 'image'
-    id = Column(Integer, primary_key = True)
-    name = Column(String)
-    
-    def __init__(self, name):
-        self.name = name
-        
-    def __repr__(self):
-        return "<Image('%s')>" % (self.name)
-        
-class Group(Base):
-    __tablename__ = 'group'
-    id = Column(Integer, primary_key = True)
-    name = Column(String)
-    
-    def __init__(self, name):
-        self.name = name
-        
-    def __repr__(self):
-        return "<Group('%s')>" % (self.name)
-
-class ImageGroup(Base):
-    __tablename__ = 'image_group'
-    image_id = Column(Integer, ForeignKey('image.id'), primary_key = True)
-    group_id = Column(Integer, ForeignKey('group.id'), primary_key = True)
-
-    def __init__(self, image_id, group_id):
-        self.image_id = image_id
-        self.group_id = group_id
-
-class OperationType(Base):
-    __tablename__ = 'operation_type'
-    id = Column(Integer, primary_key = True)
-    name = Column(String)
-
-    def __init__(self, name):
-        self.name = name
-
-class Operation(Base):
-    __tablename__ = 'operation'
-    id = Column(Integer, primary_key = True)
-    name = Column(String)
-    operation_type_id = Column(Integer, ForeignKey('operation_type.id'))
-
-    def __init__(self, name, operation_type_id):
-        pass
-
-class OperationGroup(Base):
-    __tablename__ = 'operation_group'
-    operation_id = Column(Integer, ForeignKey('operation.id'), primary_key = True)
-    group_id = Column(Integer, ForeignKey('group.id'), primary_key = True)
-
-    def __init__(self, operation_id, group_id):
-        self.operation_id = operation_id
-        self.group_id = group_id
-
-metadata = Base.metadata
-metadata.create_all(engine)
-
+create_db(engine)
 Session = sessionmaker(bind = engine)
 image = Image('Hello')
 session = Session()
 session.add(image)
 session.commit()
 
-t = session.query(Image).order_by(Image.id)
+t = session.query(Image).order_by(Image.id)[0]
 print (t)
---
+++ entities.py
@@ -0,0 +1,37 @@
+class Image(object):
+    
+    def __init__(self, name):
+        self.name = name
+        
+    def __repr__(self):
+        return "<Image('%s')>" % (self.name)
+        
+class Group(object):
+    
+    def __init__(self, name):
+        self.name = name
+        
+    def __repr__(self):
+        return "<Group('%s')>" % (self.name)
+
+class ImageGroup(object):
+
+    def __init__(self, image_id, group_id):
+        self.image_id = image_id
+        self.group_id = group_id
+
+class OperationType(object):
+
+    def __init__(self, name):
+        self.name = name
+
+class Operation(object):
+
+    def __init__(self, name, operation_type_id):
+        pass
+
+class OperationGroup(object):
+
+    def __init__(self, operation_id, group_id):
+        self.operation_id = operation_id
+        self.group_id = group_id
