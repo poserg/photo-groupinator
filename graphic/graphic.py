f57f4f986e5630e38965e725c9ea88200d6b8ec0
Sergey Popov <poserg@gmail.com>
2014-03-28 22:28:42 +0400

    Added scripts for converting images

--- graphic/graphic.py
+++ graphic/graphic.py
@@ -1,14 +1,60 @@
 # -*- coding: utf-8 -*-
 
 from PIL import Image
+from subprocess import call
 
-#convert $ftmp -gamma 0.454545 -geometry "$maxfull[0]x$maxfull[1]>" -print %w\n%h -gamma 2.2 +profile !icc* -quality $imgq "$out/$fimg" 
+MAIN_IMAGE = ("main", "1600x1067")
+THUMBS_IMAGE = ("thumbs", "168x112")
+BLUR_IMAGE = ("blur")
 
-# Generate main image
-# convert -quality 90 -resize 1600x1067 IMG_1203.JPG my_out.jpg
+def get_dir(path):
+    os.makedirs(path, exist_ok = True)
+    # d = os.path.dirname(path)
+    # if not os.path.exists(d):
+    #     os.makedirs(d)
+        
+    # return d
 
-# Thumbs
-# convert -quality 90 -resize 168x112 IMG_1203.JPG my_out.jpg
 
-# Blur
-# convert my_out.jpg -virtual-pixel Mirror -gaussian-blur 0x8 -scale 252x336 -quality 90 my_out_blur.jpg
+def resize_image(image_path, t):
+    """Generate main image
+    convert -quality 90 -resize 1600x1067 IMG_1203.JPG my_out.jpg
+    
+    Thumbs
+    convert -quality 90 -resize 168x112 IMG_1203.JPG my_out.jpg"""
+    get_dir(t[0])
+    params = [
+        "-resize",
+        t[1],
+        image_path,
+        t[0] + "/" + os.path.basename(image_path)
+        ]
+    convert_image(params)
+    
+def blur_image(image_path):
+    """Blur
+    convert my_out.jpg -virtual-pixel Mirror -gaussian-blur 0x8 -scale 252x336 -quality 90 my_out_blur.jpg"""
+    get_dir("blur")
+    params = ["-virtual-pixel",
+              "Mirror",
+              "-gaussian-blur",
+              "0x8",
+              "-scale"
+              "252x336"]
+    convert_image(params)
+    
+def convert_image(additional_params):
+    params =["convert",
+             "-quality",
+             "90"
+             ]
+    params += additional_params
+    
+    retcode = call(params)
+    
+    if recode != 0:
+        print "Error with coverting file: " + additional_params[-2]
+    
+
+def find_duplicate(path):
+    
