f59c003019bde3d594d31dcadcd755c9af0e5cf0
Sergey Popov <poserg@gmail.com>
2014-08-13 11:19:53 +0400

    Added TODO

--- main.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-
-from graphic.convert import *
-import os
-
-image = os.path.abspath('_LST8799.jpg')
-blur_image(image)
-resize_image(image, THUMBS_IMAGE)
